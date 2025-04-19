d618cf0c39a3396543abcef2189aa9f31651a6b0
Pallavi <167197400+pallavigunjal06@users.noreply.github.com>
2025-04-19 08:23:00 -0700

    volume.java

---
+++ volume.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class volume{
+        int l=10;
+        int b=20;
+        int h=5;
+        public void display()
+          {
+              System.out.println("THE VOLUME IS:"+(l*b*h));
+          }
+ }
