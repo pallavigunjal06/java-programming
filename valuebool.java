ae1bc5a22ae81ab8b3128d549dc2f513be5a517e
Pallavi <167197400+pallavigunjal06@users.noreply.github.com>
2025-04-19 08:55:25 -0700

    Create valuebool.java

---
+++ valuebool.java
@@ -0,0 +1,10 @@
+class VALUEBOOL{
+       public static void main(String args[])
+       {
+       boolean value;
+       value=true;
+       System.out.println("value is\t "+value);
+       value=false;
+       System.out.println("value is\t "+value);
+       }
+ }      
