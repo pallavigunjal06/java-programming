abd6395cca21506e267fa89d1ad09f50a2ff62ec
Pallavi <167197400+pallavigunjal06@users.noreply.github.com>
2025-04-19 08:22:10 -0700

    .java

---
+++ rectangle.java
@@ -0,0 +1,9 @@
+package pack;
+ public class rectangle{
+      double l,b;
+      public double rect(double l,double b){
+           double len=l;
+          double bre=b;
+          return(len*bre);
+        }
+}
