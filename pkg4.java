de86f41ff32c2b4e09d1881646bc94954efe1f18
Pallavi <167197400+pallavigunjal06@users.noreply.github.com>
2025-04-19 09:27:35 -0700

    Create  pkg4.java

---
+++ pkg4.java
@@ -0,0 +1,10 @@
+import pack.*;
+
+public class pkg4{
+        public static void main(String args[])
+        {
+               rectangle obj=new rectangle();
+               double arr=obj.rect(10,20);
+               System.out.println("the area of the rectangle is:"+arr);
+        }
+}
