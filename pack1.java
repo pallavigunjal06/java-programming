339dd008bdd8fc36f5eb3f10931cabaff2e77603
Pallavi <167197400+pallavigunjal06@users.noreply.github.com>
2025-04-19 08:15:40 -0700

    Create pack1.java

---
+++ pack1.java
@@ -0,0 +1,13 @@
+package pack;
+public class A{
+    public void msg()
+    {
+        System.out.println("hello java package successfully imported");
+    }
+}package pack;
+public class A{
+    public void msg()
+    {
+        System.out.println("hello java package successfully imported");
+    }
+}
